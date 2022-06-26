2956896de2d47f98ec7648c8323d71b4f8552a4e
underground0930 <go.nishiduka.1985@gmail.com>
2022-06-26 17:03:00 +0900

    reportWebVitals 修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
       break
     case 'Next.js-hydration':
       console.log(
-        `Hydration: ${metric.value.toFixed(2)} -> ${Math.round(
+        `Hydration: ${metric.startTime.toFixed(2)} -> ${Math.round(
           metric.startTime + metric.value
         ).toFixed(2)}`
       )
